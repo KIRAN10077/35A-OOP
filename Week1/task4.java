17fb1b0a9c17f7d5f13ac6f7367a5240b711b0b0
KIRAN10077 <kasalawotkiran@gmail.com>
2024-04-15 07:05:31 +0545

    task5

--- Week1/task4.java
+++ Week1/task4.java
@@ -1,5 +1,3 @@
-package task.Week1;
-
 public class task4 {
     public static void main(String[] args) {
         System.out.print("*");
---
+++ Week1/task5.java
@@ -0,0 +1,9 @@
+public class task5{
+    public static void main(String[] args) {
+        System.out.print("*********");
+        System.out.print("*********");
+        System.out.print("*********");
+        System.out.print("*********");
+        
+    }
+}
